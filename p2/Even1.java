ba2ed1ed423cedcfa376254bae3a87a1a398bb33
Harshitha-gbb <hg763615@gmail.com>
2025-08-13 14:13:54 +0530

    Even numbers

---
+++ p2/Even1.java
@@ -0,0 +1,13 @@
+package com.sgtesting.programs.p2;
+
+public class Even1 {
+    public static void main(String[] args) {
+        for(int i=2;i<=20;i++)
+        {
+            if(i%2==0)
+            {
+                System.out.println(i);
+            }
+        }
+    }
+}
