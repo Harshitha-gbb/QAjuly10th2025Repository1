ee3b622b53a9449e7279d2590e7f3a02f6a99356
Harshitha-gbb <hg763615@gmail.com>
2025-08-13 13:50:45 +0530

    Even numbers

---
+++ p1/Even.java
@@ -0,0 +1,13 @@
+package com.sgtesting.programs.p1;
+
+public class Even {
+    public static void main(String[] args) {
+        for(int i=20;i<=100;i++)
+        {
+            if(i%2==0)
+            {
+                System.out.println("Even numbers are:"+i);
+            }
+        }
+    }
+}
